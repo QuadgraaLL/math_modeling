530dc846bf9e3ed40f51246c29570a58d06e8ec6
QuadgraaLL <114813349+QuadgraaLL@users.noreply.github.com>
2022-10-10 18:51:57 +0000

    task 4 polishing

--- lab_2_task_4.py
+++ lab_2_task_4.py
@@ -2,10 +2,11 @@ f1 = 1
 f2 = 1
 n  = int(input())
 i = 0
-print(1, 1,  sep = ' ', end=' ' )
+x = [1, 1]
 while i < n-2:
   f = f1 + f2
-  print(f)
+  x.append(f)
   f1 = f2
   f2 = f
   i += 1
+print(' '.join([str(i) for i in x]))
